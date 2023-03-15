19c3d0fe88905007cd8aafe2f306c8fd86b93b2b
CJF-ZA <cjf1574828008@gmail.com>
2023-03-15 18:55:07 +0200

    First Commit
    Just set up the repository and have dompted the completed code here

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿/*Jiafei Chen
+ * ST10187612
+ * PROG6221
+ * ICE Task 1
+*/
+class ScriptDistrabutor
+{
+    public int numOfScripts { get => _numOfScripts; set => _numOfScripts = value > 0 ? value : 0; }
+    private int _numOfScripts;
+    public int numOfQ { get => _numOfQ; set => _numOfQ = (value >= 1 && value <= 10) ? value : 0; }
+    private int _numOfQ;
+    public int subTotal { get => _subTotal; set => _subTotal = value > 0 ? value : 0; }
+    private int _subTotal;
+    public int total;
+    public int numOfLec { get => _numOfLec; set => _numOfLec = value > 0 ? value : 0; }
+    private int _numOfLec;
+    
+    public void DisplayReport()
+    {
+        int numPerLec = _numOfScripts / _numOfLec;
+        int extraScript = _numOfScripts % _numOfLec;
+        Console.WriteLine($"There are {numPerLec} scripts per lecturers");
+        if (extraScript != 0)
+        {
+            Console.WriteLine($"There are {extraScript} scripts extra for the last lecturer");
+        }
+        double hours = CalcTime(numPerLec);
+        hours += CalcTime(extraScript);
+        double minutes = hours * 60;
+        double seconds = minutes * 60;
+        minutes = minutes % 60;
+        seconds = seconds % 60;
+        if (seconds > 30)
+        {
+            minutes++;
+        }
+        Console.WriteLine($"The time needed to mark all the scripts is {Math.Floor(hours)} hours {Math.Floor(minutes)} minutes");
+        Console.ReadLine();
+    }
+    private double CalcTime(int ns)
+    {
+        int numOfTicks = ns * _numOfQ * total;
+        double seconds = numOfTicks * 2;
+        double hours = seconds /60/60;
+        return hours;
+    }
+}
+
+class ScriptDistrabutorApp
+{
+    public static void Main(string[] args)
+    {
+        var sd = new ScriptDistrabutor();
+        Console.WriteLine("****************************************************");
+        Console.WriteLine("Thank you for using the Script Distrabutor");
+        Console.WriteLine("****************************************************");
+        while (sd.numOfScripts == 0)
+        {
+            Console.WriteLine("Please enter the number of scripts (Can't be 0 or negative)");
+            sd.numOfScripts = Convert.ToInt32(Console.ReadLine());
+        }
+        while (sd.numOfQ == 0)
+        {
+            Console.WriteLine("Please enter the number of questions on the script (From 1 to 10)");
+            sd.numOfQ = Convert.ToInt32(Console.ReadLine());
+        }
+        for (int i = 0; i < sd.numOfQ; i++)
+        {
+            Console.WriteLine($"Please enter the subtotal for Question {i + 1} (Can't be 0 or negative)");
+            while (sd.subTotal == 0)
+            {
+                sd.subTotal = Convert.ToInt32(Console.ReadLine());
+            }
+            sd.total += sd.subTotal;
+            sd.subTotal = 0;
+        }
+        while(sd.numOfLec == 0)
+        {
+            Console.WriteLine("Please enter the number of lecturers marking the scripts (Can't be 0 or negative)");
+            sd.numOfLec = Convert.ToInt32(Console.ReadLine());
+        }
+        sd.DisplayReport();
+    }
+}
